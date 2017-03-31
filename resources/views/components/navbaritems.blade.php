2afe41d45326ee19aa573a4df328685c8213eb07
Silver Ibenye <silver.ibenye@forrent.com>
2017-03-31 14:49:31 -0400

    Change Login/Register menu to an icon when viewed in small screen

--- resources/views/components/navbaritems.blade.php
+++ resources/views/components/navbaritems.blade.php
@@ -1,23 +1,29 @@
 <!-- Authentication Links -->
 @if (Auth::guest())
-    <li><a href="{{ route('login') }}">Login</a></li>
-    <li><a href="{{ route('register') }}">Register</a></li>
-    @else
-        <button id="demo-menu-lower-right"
-        class="mdl-button mdl-js-button mdl-button--icon">
-          <i class="material-icons">account_circle</i>
-        </button>
-
-<ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
-    for="demo-menu-lower-right">
-  <li class="mdl-menu__item"><a href="{{ route('account') }}">My Account</a></li>
-  <li class="mdl-menu__item"><a href="{{ route('logout') }}"
-                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                    Logout </a>
-
-            <form id="logout-form" action="{{ route('logout') }}"method="POST" style="display: none;">
-            {{ csrf_field() }}
-            </form></li>
-</ul>
-
-    @endif
+    <li class="nsh-guest-menu-item"><a href="{{ route('login') }}">Login</a></li>
+    <li class="nsh-guest-menu-item"><a href="{{ route('register') }}">Register</a></li>
+    <div class="dropdown nsh-guest-dropdown-menu">
+      <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+        <i class="material-icons">&#xE5D2;</i>
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+        <li class="nsh-guest-dropdown-menu-item"><a href="{{ route('login') }}">Login</a></li>
+        <li class="nsh-guest-dropdown-menu-item"><a href="{{ route('register') }}">Register</a></li>
+      </ul>
+    </div>
+@else
+    <div class="dropdown nsh-user-dropdown-menu">
+      <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+        <i class="material-icons">&#xE853;</i>
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+        <li class="nsh-user-dropdown-menu-item"><a href="{{ route('account') }}">My Account</a></li>
+        <li class="nsh-user-dropdown-menu-item"><a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        Logout </a>
+                        <form id="logout-form" action="{{ route('logout') }}"method="POST" style="display: none;">
+                {{ csrf_field() }}
+                </form></li>
+      </ul>
+    </div>
+@endif
